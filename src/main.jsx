fbcd1703323df5628a321fde2449c5cc7ba35d54
anuragchallapalli1987 <anuragchallapalli@gmail.com>
2026-06-26 23:01:18 +0530

    Add React entrypoint

---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import { createRoot } from "react-dom/client";
+import App from "../AnuragOS_Command_Center.jsx";
+
+createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
